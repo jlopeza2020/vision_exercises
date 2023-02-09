905693d0fcce0c56d860c954ccd2c4aa2aa4c3aa
Julia López <j.lopeza.2020@alumnos.urjc.es>
2023-02-09 22:29:12 +0100

    p1 wip

--- cv_node_p1.cpp
+++ cv_node_p1.cpp
@@ -44,15 +44,15 @@ class ComputerVisionSubscriber : public rclcpp::Node
       "/head_front_camera/rgb/image_raw", qos, std::bind(&ComputerVisionSubscriber::topic_callback, this, std::placeholders::_1));
     
       publisher_ = this->create_publisher<sensor_msgs::msg::Image>(
-      "cv_image", qos); // contains an image 
+      "cv_image", qos);
     }
 
   private:
     void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
     {     
-      // Convert ROS Image to CV Image works with BGR 
+      // Convert ROS Image to CV Image
       cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-      cv::Mat image_raw =  cv_ptr->image; // accedo al puntero con los datos contenidos 
+      cv::Mat image_raw =  cv_ptr->image;
 
       // Image processing
       cv::Mat cv_image = image_processing(image_raw);
@@ -73,15 +73,83 @@ class ComputerVisionSubscriber : public rclcpp::Node
 };
 
 /**
-  TO-DO !!!!
+  TO-DO COMPLETE THIS PART 
 */
+/*cv::Mat image_inRGB(const cv::Mat image2process) 
+{
+  cv::Mat out_image;
+
+}
+
+cv::Mat image_inCMY(const cv::Mat image2process) 
+{
+  cv::Mat out_image;
+
+}
+
+cv::Mat image_inHSI(const cv::Mat image2process) 
+{
+  cv::Mat out_image;
+}
+
+cv::Mat image_inHSV(const cv::Mat image2process) 
+{
+  cv::Mat out_image;
+}
+
+cv::Mat image_inHSVOP(const cv::Mat image2process) 
+{
+  cv::Mat out_image;
+}
+
+cv::Mat image_inHSIOP(const cv::Mat image2process) 
+{
+  cv::Mat out_image;
+}*/
+
 cv::Mat image_processing(const cv::Mat in_image) 
 {
+  
   // Create output image
   cv::Mat out_image;
   
-  // Processing
+  // swith case con las 6 opciones
+  int key = cv::pollKey();
+  switch(key) {
+    case 49:
+      std::cout << "pulsado 1: RGB\n" << std::endl;
+      //out_image = image_inRGB(in_image);
+      break;
+    case 50:
+      std::cout << "pulsado 2: CMY\n" << std::endl;
+      //out_image = image_inCMY(in_image);
+      break;
+    case 51:
+      std::cout << "pulsado 3: HSI\n" << std::endl;
+      //out_image = image_inHSI(in_image);
+      break;
+    case 52:
+      std::cout << "pulsado 4: HSV\n" << std::endl;
+      //out_image = image_inHSV(in_image);
+      break;
+    case 53:
+      std::cout << "pulsado 5: HSV OpenCV\n" << std::endl;
+      //out_image = image_inHSVOP(in_image);
+      break;
+    case 54:
+      std::cout << "pulsado 6: HSI OpenCV\n" << std::endl;
+      //out_image = image_inHSIOP(in_image);
+      break;
+  }
+
+  
+  // Processing (output image = input image)
   out_image = in_image;
+  
+  // Write text in an image
+  cv::String text = "1:RGB, 2:CMY, 3:HSI, 4:HSV, 5: HSV OpenCV, 6: HSI OpenCV";
+  cv::putText(out_image, text , cv::Point(10, 20),
+  cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 255));
 
   // Show image in a different window
   cv::imshow("out_image",out_image);
