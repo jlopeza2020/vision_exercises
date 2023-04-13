bf5483c8a40e7493a992573167dc9e04082d0afd
Julia <jlopeza2020@alumnos.urjc.es>
2023-04-13 22:04:54 +0200

    get extrinsic matrix

--- cv_node_p5.cpp
+++ cv_node_p5.cpp
@@ -22,18 +22,71 @@
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include <image_transport/image_transport.hpp>
 #include "image_geometry/pinhole_camera_model.h"
-//#include <tf2_ros/transform_listener.h>
-//#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-//#include "sensor_msgs/msg/CameraInfo.hpp"
+
+
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "geometry_msgs/msg/transform_stamped.h"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
 
 #include "cv_bridge/cv_bridge.h"
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include <tf2/LinearMath/Transform.h>
+#include <Eigen/Dense>
+
+using namespace std::chrono_literals;
+
 int key;
 bool print_once = true;
 cv::Matx33f K; //intrinsic values 
+
+//geometry_mesgs::msg::TransformStamped t;
+geometry_msgs::msg::TransformStamped extrinsic; 
+
+cv::Mat extrinsic_matrix;
+
+//Eigen::Affine3d extrinsicMatrix;
+//Eigen::Isometry3d transformEigen;
+//Eigen::Matrix4d extrinsiceigen;
+//tf2::fromMsg(extrinsic.transform, extrinsicMatrix);
+
+/*auto& trans_matrix = transform_stamped.transform.translation;
+auto& rot_matrix = transform_stamped.transform.rotation;
+double x_var = trans_matrix.x;
+double y_var = trans_matrix.y;
+double z_var = trans_matrix.z;
+
+double qw = rot_matrix.w;
+double qx = rot_matrix.x;
+double qy = rot_matrix.y;
+double qz = rot_matrix.z;*/
+
+// Convertir la TransformStamped a una matriz de transformación
+/*tf2::Matrix3x3 rotation_matrix;
+tf2::fromMsg(extrinsic.transform.rotation, rotation_matrix);
+tf2::Vector3 translation_vector;
+tf2::fromMsg(extrinsic.transform.translation, translation_vector);
+cv::Mat rvec, tvec;
+cv::Rodrigues(cv::Mat(rotation_matrix), rvec);
+tvec = cv::Mat(translation_vector[0], translation_vector[1], translation_vector[2]);
+
+// Combinar la matriz de rotación y la matriz de traslación en una matriz de transformación
+cv::Mat transformation_matrix = cv::Mat::eye(4, 4, CV_64F);
+cv::Mat rotation_matrix_cv;
+cv::Rodrigues(rvec, rotation_matrix_cv);
+rotation_matrix_cv.copyTo(transformation_matrix(cv::Rect(0, 0, 3, 3)));
+tvec.copyTo(transformation_matrix(cv::Rect(3, 0, 1, 3)));
+
+// Invertir la matriz de transformación para obtener la matriz de transformación extrínseca
+cv::Mat extrinsic_matrix = transformation_matrix.inv();*/
+
+
+
 
 cv::Mat image_processing(const cv::Mat in_image);
 
@@ -55,11 +108,14 @@ class ComputerVisionSubscriber : public rclcpp::Node
     
     
       // Inicializar el transform listener
-      //tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
-      //tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+      tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+      tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-      publisher_ = this->create_publisher<sensor_msgs::msg::Image>(
-      "cv_image", qos);
+      // Call on_timer function every 500ms
+      timer_ = this->create_wall_timer(500ms, std::bind(&ComputerVisionSubscriber::on_timer, this));
+
+
+      publisher_ = this->create_publisher<sensor_msgs::msg::Image>("cv_image", qos);
     }
 
   private:
@@ -85,39 +141,44 @@ class ComputerVisionSubscriber : public rclcpp::Node
     void topic_callback_in_params(const sensor_msgs::msg::CameraInfo::SharedPtr msg) const
     {     
       // Convert ROS Image to CV Image
-      //cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-      //cv::Mat image_raw =  cv_ptr->image;
-
-      // Image processing
-      //cv::Mat cv_image = image_processing(image_raw);
 
       // Crear modelo de cámara
       image_geometry::PinholeCameraModel camera_model = image_geometry::PinholeCameraModel();
       camera_model.fromCameraInfo(msg);
 
-
       // Obtener matriz de parámetros intrínsecos
       K = camera_model.intrinsicMatrix();
 
-      // Obtener transformada del punto 4 a la base_footprint
-      //geometry_msgs::msg::TransformStamped transform_stamped;
+    }
 
+    void on_timer(){
       //transform_stamped = tf_buffer_.lookupTransform( "head_front_camera_rgb_optical_frame", "base_footprint", tf2::TimePointZero);
-  
+      try {
+        extrinsic = tf_buffer_->lookupTransform("head_front_camera_rgb_optical_frame", "base_footprint", tf2::TimePointZero);
+      } catch (tf2::TransformException &ex) {
+        RCLCPP_WARN(this->get_logger(), "Failed to lookup transform: %s", ex.what());
+        return;
+      }
 
-      // Convert OpenCV Image to ROS Image
-      //cv_bridge::CvImage img_bridge = cv_bridge::CvImage(msg->header, sensor_msgs::image_encodings::BGR8, cv_image);
-      //sensor_msgs::msg::Image out_image; // >> message to be sent
-      //img_bridge.toImageMsg(out_image); // from cv_bridge to sensor_msgs::Image
+      //tf2::fromMsg(extrinsic.transform, extrinsicMatrix);
+      //tf2::fromMsg(extrinsic.transform, transformEigen);
+      //extrinsiceigen = transformEigen.matrix().inverse();
 
-      // Publish the data
-      //publisher_ -> publish(out_image);
 
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
     rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr subscription_info_;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+
+    //tf2_ros::Buffer tf_buffer_;
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    //tf2::BufferCore tf_buffer_;
+   //tf2_ros::TransformListener tf_listener_;
+
    
 };
 
@@ -131,8 +192,6 @@ std::vector<cv::Point> points;
 
 void on_mouse(int event, int x, int y, int, void*)
 {
-  //switch (event){
-
   if (event == cv::EVENT_LBUTTONDOWN){
     points.push_back(cv::Point(x, y));
   }
@@ -145,7 +204,7 @@ void on_mouse(int event, int x, int y, int, void*)
     //default:
     //  std::cout << "No button" << std::endl;
     //  break;
-  //}
+    //}
 }
 
 cv::Mat detect_skeleton(cv::Mat in_image, int iters){
@@ -188,20 +247,10 @@ cv::Mat detect_skeleton(cv::Mat in_image, int iters){
     }
   }
 
-  //THIS WORKS
-  /*for (uint i = 0; i < points.size(); i++) {
-    circle(out_image, points[i], 3, cv::Scalar(0, 0, 255), -1);
-  }*/
-
   return in_clone;
 
 
 }
-/*void from_3Dto2D(const cv::Mat image){
-
-
-  
-}*/
 
 
 cv::Mat image_processing(const cv::Mat in_image) 
@@ -225,6 +274,7 @@ cv::Mat image_processing(const cv::Mat in_image)
     cv::setTrackbarPos("Iterations", "P5", init_value_iters);
     cv::createTrackbar("Distance", "P5", nullptr, max_value_distance, 0);
     cv::setTrackbarPos("Distance", "P5", init_value_distance);
+
     print_once = false;
   }
 
@@ -254,36 +304,50 @@ cv::Mat image_processing(const cv::Mat in_image)
         circle(out_image, points[i], 3, cv::Scalar(0, 0, 255), -1);
       }
     
-      std::cout << K << std::endl;
-      /*ros::NodeHandle nh;
+      //std::cout << K << std::endl;
 
-      // Creación de suscriptor para el topic de información de la cámara
-      ros::Subscriber camera_info_sub = nh.subscribe("/head_front_camera/rgb/camera_info", 1, cameraInfoCallback);
+      extrinsic_matrix = (cv::Mat_<double>(3,4) << 
+                                    1, 0, 0, extrinsic.transform.translation.x,
+                                    0, 1, 0, extrinsic.transform.translation.y,
+                                    0, 0, 1, extrinsic.transform.translation.z);
 
-      // Creación del objeto para obtener los parámetros extrínsecos
-      tf2_ros::Buffer tf_buffer;
-      tf2_ros::TransformListener tf_listener(tf_buffer);
-
-      // Loop de ROS
-      //ros::Rate rate(10.0);
-      //while (ros::ok())
-      //{
-    // Obtener los parámetros extrínsecos
-      try
-      {
-        geometry_msgs::TransformStamped  transform_stamped = tf_buffer_.lookupTransform( "head_front_camera_rgb_optical_frame", "base_footprint", tf2::TimePointZero);
-
-        //geometry_msgs::TransformStamped transformStamped = tf_buffer.lookupTransform("", "point_4", ros::Time(0));
-        Eigen::Matrix4d extrinsic_matrix;
-        tf2::fromMsg(transformStamped.transform, extrinsic_matrix);
-      }
-      catch (tf2::TransformException &ex)
-      {
-        ROS_WARN("%s", ex.what());
-      }*/
+      std::cout << extrinsic.transform.translation.x << std::endl;
+      std::cout << extrinsic.transform.translation.y << std::endl;
+      std::cout << extrinsic.transform.translation.z << std::endl;
 
 
+      std::cout << extrinsic_matrix << std::endl;
+      //geometry_msgs::msg::Quaternion rotation = extrinsic.transform.rotation;
 
+      //tf2::Quaternion tf_quaternion(rotation.x, rotation.y, rotation.z, rotation.w);
+      //tf2::Matrix3x3 tf_rotation_matrix;
+      //tf_rotation_matrix.setRotation(tf_quaternion);
+
+
+      //auto& rot_matrix = transform_stamped.transform.rotation;
+      //auto& trans_matrix = transform_stamped.transform.translation;
+
+      // Acceder a los componentes de la matriz de rotación y la matriz de traslación
+      //double x_var = trans_matrix.x;
+
+      /*std::cout << x_var << std::endl;
+      std::cout << y_var << std::endl;
+      std::cout << z_var << std::endl;*/
+      /*double y = trans_matrix.y;
+      double z = trans_matrix.z;
+
+
+
+      double qw = rot_matrix.w;
+      double qx = rot_matrix.x;
+      double qy = rot_matrix.y;
+      double qz = rot_matrix.z;*/
+
+      /*std::cout << qw << std::endl;
+      std::cout << qx << std::endl;
+      std::cout << qy << std::endl;*/
+
+      
       break;
 
     case 2:
