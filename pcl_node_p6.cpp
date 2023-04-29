3829225b9f9e6d02c62551f492bc44766424cf00
Julia <jlopeza2020@alumnos.urjc.es>
2023-04-29 10:30:02 +0200

    inlier ball

--- pcl_node_p6.cpp
+++ pcl_node_p6.cpp
@@ -39,6 +39,10 @@
 #include <pcl/filters/conditional_removal.h>
 #include <pcl/filters/passthrough.h>
 
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+
 int key;
 bool print_once = true;
 pcl::PointCloud<pcl::PointXYZRGB> pcl_processing(const pcl::PointCloud<pcl::PointXYZRGB> in_pointcloud);
@@ -112,7 +116,7 @@ void PointCloudXYZHSV2XYZRGB(pcl::PointCloud<pcl::PointXYZHSV>& in, pcl::PointCl
 }
  
 
-pcl::PointCloud<pcl::PointXYZRGB> get_hsv(pcl::PointCloud<pcl::PointXYZRGB> cloud_in, int h_min, int s_min,int v_min, int h_max, int s_max, int v_max){
+/*pcl::PointCloud<pcl::PointXYZRGB> get_hsv(pcl::PointCloud<pcl::PointXYZRGB> cloud_in, int h_min, int s_min,int v_min, int h_max, int s_max, int v_max){
 //pcl::PointCloud<pcl::PointXYZHSV> get_hsv(pcl::PointCloud<pcl::PointXYZRGB> cloud){
 
   pcl::PointCloud<pcl::PointXYZRGB> cloud_out;
@@ -148,6 +152,96 @@ pcl::PointCloud<pcl::PointXYZRGB> get_hsv(pcl::PointCloud<pcl::PointXYZRGB> clou
   PointCloudXYZHSV2XYZRGB(*cloud_hsv, cloud_out);
 
   return cloud_out;
+}*/
+
+pcl::PointCloud<pcl::PointXYZRGB> get_hsv(pcl::PointCloud<pcl::PointXYZRGB> cloud_in){
+//pcl::PointCloud<pcl::PointXYZHSV> get_hsv(pcl::PointCloud<pcl::PointXYZRGB> cloud){
+
+  pcl::PointCloud<pcl::PointXYZRGB> cloud_out;
+
+  // Convertir de RGB a HSV
+  pcl::PointCloud<pcl::PointXYZHSV>::Ptr cloud_hsv(new pcl::PointCloud<pcl::PointXYZHSV>);
+  //PointXYZRGB2XYZHSV(cloud_in, *cloud_hsv);
+  PointCloudXYZRGB2XYZHSV(cloud_in, *cloud_hsv);
+
+  // Crear una condición para filtrar los puntos de color rosa
+  pcl::ConditionAnd<pcl::PointXYZHSV>::Ptr color_cond(new pcl::ConditionAnd<pcl::PointXYZHSV>);
+  color_cond->addComparison(pcl::FieldComparison<pcl::PointXYZHSV>::ConstPtr(
+                                 new pcl::FieldComparison<pcl::PointXYZHSV>("h", pcl::ComparisonOps::GE, 297)));
+  color_cond->addComparison(pcl::FieldComparison<pcl::PointXYZHSV>::ConstPtr(
+                                 new pcl::FieldComparison<pcl::PointXYZHSV>("h", pcl::ComparisonOps::LE, 300)));
+  color_cond->addComparison(pcl::FieldComparison<pcl::PointXYZHSV>::ConstPtr(
+                                 new pcl::FieldComparison<pcl::PointXYZHSV>("s", pcl::ComparisonOps::GE, 254/255)));
+  color_cond->addComparison(pcl::FieldComparison<pcl::PointXYZHSV>::ConstPtr(
+                                 new pcl::FieldComparison<pcl::PointXYZHSV>("s", pcl::ComparisonOps::LE, 255/255)));
+  color_cond->addComparison(pcl::FieldComparison<pcl::PointXYZHSV>::ConstPtr(
+                                 new pcl::FieldComparison<pcl::PointXYZHSV>("v", pcl::ComparisonOps::GE, 254/255)));
+  color_cond->addComparison(pcl::FieldComparison<pcl::PointXYZHSV>::ConstPtr(
+                                 new pcl::FieldComparison<pcl::PointXYZHSV>("v", pcl::ComparisonOps::LE, 255/255)));
+
+  // Aplicar el filtro de condición para filtrar los puntos de color rosa
+  pcl::ConditionalRemoval<pcl::PointXYZHSV> condrem;
+  condrem.setCondition(color_cond);
+  condrem.setInputCloud(cloud_hsv);
+  condrem.setKeepOrganized(true);
+  condrem.filter(*cloud_hsv);
+
+  // Convertir de vuelta de HSV a RGB
+  PointCloudXYZHSV2XYZRGB(*cloud_hsv, cloud_out);
+
+  return cloud_out;
+}
+
+
+/*pcl::PointCloud<pcl::PointXYZRGB> get_inlier(pcl::PointCloud<pcl::PointXYZRGB> cloud)
+{
+  ///pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+  // Fill in the cloud data
+  //pcl::PCDReader reader;
+  // Replace the path below with the path where you saved your file
+  //reader.read<pcl::PointXYZ> ("table_scene_lms400.pcd", *cloud);
+
+  //std::cerr << "Cloud before filtering: " << std::endl;
+  //std::cerr << *cloud << std::endl;
+
+  // Create the filtering object
+  //pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
+  sor.setInputCloud (cloud);
+  sor.setMeanK (50);
+  sor.setStddevMulThresh (1.0);
+  sor.filter (*cloud_filtered);
+
+  //std::cerr << "Cloud after filtering: " << std::endl;
+  //std::cerr << *cloud_filtered << std::endl;
+
+  //pcl::PCDWriter writer;
+  //writer.write<pcl::PointXYZ> ("table_scene_lms400_inliers.pcd", *cloud_filtered, false);
+
+  ///sor.setNegative (true);
+  //sor.filter (*cloud_filtered);
+  //writer.write<pcl::PointXYZ> ("table_scene_lms400_outliers.pcd", *cloud_filtered, false);
+
+  return *cloud_filtered;
+}*/
+
+pcl::PointCloud<pcl::PointXYZRGB> removeColorOutliers(pcl::PointCloud<pcl::PointXYZRGB> cloud)
+{
+  pcl::PointCloud<pcl::PointXYZRGB> cloud_filtered;
+
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
+  sor.setInputCloud(cloud.makeShared());
+  //sor.setInputColors(cloud.makeShared());
+  sor.setMeanK(50); // Establecer el número de vecinos a considerar para el cálculo de la desviación estándar
+  sor.setStddevMulThresh(1.0); // Establecer el umbral para eliminar los outliers
+  //sor.setColorThreshold(50); // Establecer el umbral para eliminar los outliers de color
+  sor.filter(cloud_filtered);
+
+  return cloud_filtered;
 }
 
 
@@ -156,11 +250,12 @@ pcl::PointCloud<pcl::PointXYZRGB> pcl_processing(const pcl::PointCloud<pcl::Poin
 {
   // Create output pointcloud
   pcl::PointCloud<pcl::PointXYZRGB> out_pointcloud;
+  pcl::PointCloud<pcl::PointXYZRGB> outlier_pointcloud;
 
   // Processing
   //out_pointcloud = in_pointcloud;
 
-  cv::Mat test = cv::Mat::zeros(cv::Size(600,100), CV_32FC1);
+  /*cv::Mat test = cv::Mat::zeros(cv::Size(600,100), CV_32FC1);
 
   int max_h = 360;
   int max_s =  255;
@@ -196,13 +291,16 @@ pcl::PointCloud<pcl::PointXYZRGB> pcl_processing(const pcl::PointCloud<pcl::Poin
   int gt_max_v = cv::getTrackbarPos("max V", "P5");
   int gt_min_h = cv::getTrackbarPos("min H", "P5");
   int gt_min_s = cv::getTrackbarPos("min S", "P5");
-  int gt_min_v = cv::getTrackbarPos("min V", "P5");
+  int gt_min_v = cv::getTrackbarPos("min V", "P5");*/
 
 
 
-  out_pointcloud = get_hsv(in_pointcloud, gt_min_h, gt_min_s ,gt_min_v, gt_max_h, gt_max_s, gt_max_v);
+  //out_pointcloud = get_hsv(in_pointcloud, gt_min_h, gt_min_s ,gt_min_v, gt_max_h, gt_max_s, gt_max_v);
+  outlier_pointcloud = get_hsv(in_pointcloud);
+  out_pointcloud = removeColorOutliers(outlier_pointcloud);
 
-  cv::imshow("P5",test);
+
+  //cv::imshow("P5",test);
 
 
   return out_pointcloud;
